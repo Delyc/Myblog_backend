d3065b7eced97e5bf31f9e72a8cfa06d29d28809
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2022-01-16 13:55:54 +0200

    authorization

--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,6 @@
 import express from "express";
 const articleRouter = express.Router();
+import { authenticate } from "../middlewares/auth.js";
 
 import {
   createArticle,
@@ -16,16 +17,30 @@ import {
 } from "../controllers/articles.js";
 
 // create routes
-articleRouter.route("/").post(createArticle);
-articleRouter.route("/:id").get(getArticleById);
-articleRouter.route("/").get(viewAllArticles);
-articleRouter.route("/:id").put(updateArticle);
-articleRouter.route("/:id").delete(deleteArticle);
-articleRouter.route("/search/:search").get(searchArticle);
-articleRouter.route("/:id/comments").post(addComment);
-articleRouter.route("/:id/comments/:user").delete(deleteComment);
-articleRouter.route("/:id/comments").get(getComments);
-articleRouter.route("/:id/likes").post(likeArticle);
-articleRouter.route("/:id/likes").get(getLikes);
+
+articleRouter.post("/", authenticate, createArticle);
+articleRouter.get("/:id", getArticleById);
+articleRouter.get("/", viewAllArticles);
+articleRouter.put("/:id", authenticate, updateArticle);
+articleRouter.delete("/:id", authenticate, deleteArticle);
+articleRouter.get("/search/:search", searchArticle);
+articleRouter.post("/:id/comments", addComment);
+articleRouter.delete("/:id/comments/:user", authenticate, deleteComment);
+articleRouter.get("/:id/comments", getComments);
+articleRouter.post("/:id/likes", likeArticle);
+articleRouter.get("/:id/likes", getLikes);
+
+
+// articleRouter.route("/").post(createArticle);
+// articleRouter.route("/:id").get(getArticleById);
+// articleRouter.route("/").get(viewAllArticles);
+// articleRouter.route("/:id").put(updateArticle);
+// articleRouter.route("/:id").delete(deleteArticle);
+// articleRouter.route("/search/:search").get(searchArticle);
+// articleRouter.route("/:id/comments").post(addComment);
+// articleRouter.route("/:id/comments/:user").delete(deleteComment);
+// articleRouter.route("/:id/comments").get(getComments);
+// articleRouter.route("/:id/likes").post(likeArticle);
+// articleRouter.route("/:id/likes").get(getLikes);
 
 export default articleRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { authenticate } from "../middlewares/auth.js";
 const userRouter = express.Router();
 
 import {
@@ -11,12 +12,22 @@ import {
   searchUser,
 } from "../controllers/users.js";
 
-userRouter.route("/:id").get(getUserById);
-userRouter.route("/").post(createUser);
-userRouter.route("/").get(getAllUsers);
-userRouter.route("/:id").delete(deleteUser);
-userRouter.route("/login").post(loginUser);
-userRouter.route("/:id").put(updateUser);
-userRouter.route("/search/:search").get(searchUser);
+// userRouter.route("/:id").get(getUserById);
+userRouter.get("/:id", getUserById);
+userRouter.post("/", authenticate, createUser);
+userRouter.get("/",  getAllUsers);
+userRouter.delete("/:id", authenticate, deleteUser);
+userRouter.post("/login", loginUser);
+userRouter.put("/:id", authenticate, updateUser);
+userRouter.get("/search/:search", searchUser);
+
+
+
+// userRouter.route("/").post(createUser);
+// userRouter.route("/").get(getAllUsers);
+// userRouter.route("/:id").delete(deleteUser);
+// userRouter.route("/login").post(loginUser);
+// userRouter.route("/:id").put(updateUser);
+// userRouter.route("/search/:search").get(searchUser);
 
 export default userRouter;
