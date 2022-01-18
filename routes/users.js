e3ddd3b55501fc11c3c53e31e91742d5b9f2afdb
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2022-01-18 18:52:54 +0200

    Add the documentation to the API
    Using swagger and swagger ui, the documentation
    are hosted to the websiite.
    
    - Components and login

--- routes/users.js
+++ routes/users.js
@@ -15,13 +15,89 @@ import {
 // userRouter.route("/:id").get(getUserById);
 userRouter.get("/:id", getUserById);
 userRouter.post("/", authenticate, createUser);
-userRouter.get("/",  getAllUsers);
+userRouter.get("/", getAllUsers);
 userRouter.delete("/:id", authenticate, deleteUser);
+/**
+ * @openapi
+ * tags:
+ *  name: User
+ *
+ */
+
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    Logininfo:
+ *      type: object
+ *      required:
+ *        - email
+ *        - password
+ *      example:
+ *        email: info@me.com
+ *        password: Me202220
+ *      properties:
+ *        email:
+ *          type: email
+ *          description: A valid email is required
+ *        password:
+ *          type: string
+ *          description: A password
+ */
+
+/**
+ * openapi
+ * /api/users:
+ *  post:
+ *    summary: login
+ *    tags:[User]
+ *    description: user login
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *           #ref: "#/components/schemas/Logininfo"
+ *      responses:
+ *        200:
+ *          description: Loggen in
+ *
+ */
+
+/**
+ * @swagger
+ * /api/users/login:
+ *  post:
+ *    summary: user login
+ *    requestBody:
+ *     required: true
+ *     contents:
+ *       application/json:
+ *         schema:
+ *           #ref: "#/components/schemas/Logininfo"
+ */
 userRouter.post("/login", loginUser);
+/**
+ * @openapi
+ * /api/users/{id}:
+ *   put:
+ *    summary: Update a user
+ *    tags:
+ *      - User
+ *    parameters:
+ *      - in : path
+ *        required: true
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            properties:
+ *               email:
+ *                  type: string
+ */
 userRouter.put("/:id", authenticate, updateUser);
 userRouter.get("/search/:search", searchUser);
-
-
 
 // userRouter.route("/").post(createUser);
 // userRouter.route("/").get(getAllUsers);
