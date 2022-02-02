1441d3609d60d44ce7d8051c3fc5d3b94b85baf7
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2022-02-03 00:12:22 +0200

    fixes

--- controllers/hireme.js
+++ controllers/hireme.js
@@ -32,7 +32,7 @@ export const hireMe = async (req, res) => {
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: "delyce2002@gmail.com", // generated ethereal user
+      user: "delycetwizeyimana@gmail.com", // generated ethereal user
       pass: process.env.MAIL_PASSWORD, // generated ethereal password
     },
   });
